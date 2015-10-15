ffdfc09ba747b2cbb0de1c3ed60eb7b55226b575
schnubor <info@chko.org>
2015-10-15 20:27:17 +0200

    added github to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,5 +4,6 @@
     <a href="{{ route('imprint') }}">Imprint</a>
     <a href="https://www.facebook.com/chko.org" target="_blank"><i class="fa fa-facebook-f"></i></a>
     <a href="https://twitter.com/schnubor" target="_blank"><i class="fa fa-twitter"></i></a>
+    <a href="https://github.com/schnubor" target="_blank"><i class="fa fa-github"></i></a>
     <span class="pull-right">© 2015</span>
 </div>
