23d237f23cfe16c45b7b544eefe2b05c9b9b438e
schnubor <info@chko.org>
2015-10-15 23:39:21 +0200

    be fix

--- resources/views/partials/backend/images.blade.php
+++ resources/views/partials/backend/images.blade.php
@@ -10,31 +10,29 @@
                         @if($projects->count())
                             @foreach($projects as $project)
                                 @if($project->images->count())
-                                <div class="well">
-                                    <legend>{{ $project->title }}</legend>
-                                    <div class="row">
-                                        @foreach($project->images as $image)
-                                            <div class="col-md-3">
-                                                <div class="thumbnail">
-                                                    <img src="{{ '/uploads/images/'.$image->filename }}" alt="Image of {{ $image->project->title }}">
-                                                    <div class="caption">
-                                                        <small style="color: #ccc;">{{ $image->filename }}</small><br>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-md-12">
-                                                            {!! Form::open(['route' => ['delete.image', $image->id], 'style' => 'margin-bottom: 0; display: inline-block;', 'onsubmit'=>'return confirm("Really want to delete that image?");', 'class' => 'pull-right']) !!}
-                                                                <input name="_method" type="hidden" value="DELETE">
-                                                                {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-sm btn-danger', 'data-toggle' => 'tooltip',  'data-placement' => 'bottom', 'title' => 'Delete']) !!}
-                                                            {!! Form::close() !!}
+                                    <div class="well">
+                                        <legend>{{ $project->title }}</legend>
+                                        <div class="row">
+                                            @foreach($project->images as $image)
+                                                <div class="col-md-3">
+                                                    <div class="thumbnail">
+                                                        <img src="{{ '/uploads/images/'.$image->filename }}" alt="Image of {{ $image->project->title }}">
+                                                        <div class="caption">
+                                                            <small style="color: #ccc;">{{ $image->filename }}</small><br>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-12">
+                                                                {!! Form::open(['route' => ['delete.image', $image->id], 'style' => 'margin-bottom: 0; display: inline-block;', 'onsubmit'=>'return confirm("Really want to delete that image?");', 'class' => 'pull-right']) !!}
+                                                                    <input name="_method" type="hidden" value="DELETE">
+                                                                    {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-sm btn-danger', 'data-toggle' => 'tooltip',  'data-placement' => 'bottom', 'title' => 'Delete']) !!}
+                                                                {!! Form::close() !!}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        @endforeach
+                                            @endforeach
                                         </div>
                                     </div>
-                                @else
-                                    <p class="lead">No images uploaded yet.</p>
                                 @endif
                             @endforeach
                         @else
--- resources/views/partials/backend/videos.blade.php
+++ resources/views/partials/backend/videos.blade.php
@@ -10,31 +10,29 @@
                         @if($projects->count())
                             @foreach($projects as $project)
                                 @if($project->videos->count())
-                                <div class="well">
-                                    <legend>{{ $project->title }}</legend>
-                                    <div class="row">
-                                        @foreach($project->videos as $video)
-                                            <div class="col-md-3">
-                                                <div class="thumbnail">
-                                                    <iframe width="100%" src="https://www.youtube.com/embed/{{ $video->youtube_id }}" frameborder="0" allowfullscreen></iframe>
-                                                    <div class="caption">
-                                                    <small style="color: #ccc;"><a href="https://www.youtube.com/watch?v={{ $video->youtube_id }}" target="_blank">Watch on youtube</a></small><br>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-md-12">
-                                                            {!! Form::open(['route' => ['delete.video', $video->id], 'style' => 'margin-bottom: 0; display: inline-block;', 'onsubmit'=>'return confirm("Really want to delete that video?");', 'class' => 'pull-right']) !!}
-                                                                <input name="_method" type="hidden" value="DELETE">
-                                                                {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-sm btn-danger', 'data-toggle' => 'tooltip',  'data-placement' => 'bottom', 'title' => 'Delete']) !!}
-                                                            {!! Form::close() !!}
+                                    <div class="well">
+                                        <legend>{{ $project->title }}</legend>
+                                        <div class="row">
+                                            @foreach($project->videos as $video)
+                                                <div class="col-md-3">
+                                                    <div class="thumbnail">
+                                                        <iframe width="100%" src="https://www.youtube.com/embed/{{ $video->youtube_id }}" frameborder="0" allowfullscreen></iframe>
+                                                        <div class="caption">
+                                                        <small style="color: #ccc;"><a href="https://www.youtube.com/watch?v={{ $video->youtube_id }}" target="_blank">Watch on youtube</a></small><br>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-12">
+                                                                {!! Form::open(['route' => ['delete.video', $video->id], 'style' => 'margin-bottom: 0; display: inline-block;', 'onsubmit'=>'return confirm("Really want to delete that video?");', 'class' => 'pull-right']) !!}
+                                                                    <input name="_method" type="hidden" value="DELETE">
+                                                                    {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-sm btn-danger', 'data-toggle' => 'tooltip',  'data-placement' => 'bottom', 'title' => 'Delete']) !!}
+                                                                {!! Form::close() !!}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        @endforeach
+                                            @endforeach
+                                        </div>
                                     </div>
-                                </div>
-                                @else
-                                    <p class="lead">No videos uploaded yet.</p>
                                 @endif
                             @endforeach
                         @else
